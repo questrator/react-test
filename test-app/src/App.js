ef3925c25d4a99b9972894a325494e96cb727c79
Slava Ivanov <drumtrack@ya.ru>
2023-03-28 17:38:52 +0300

    upd

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -3,12 +3,14 @@ import Books from './Books';
 
 function App() {  
   const books = [
-    {name: "onee", id: 12, price: 340},
+    {name: "one", id: 12, price: 340},
     {name: "two", id: 14, price: 140},
     {name: "three", id: 16, price: 240},
     {name: "four", id: 1123, price: 840},
     {name: "five", id: 456, price: 450},
     {name: "six", id: 3523, price: 257},
+    {name: "seven", id: 823, price: 27},
+    {name: "eight", id: 67, price: 118},
   ];
   return (
     <Books books={books}/>
--- test-app/src/Books.js
+++ test-app/src/Books.js
@@ -1,42 +1,93 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function Books(props) {
+    const ipp = 3;
     const order = {
         "dflt": "без сортировки",
         "ascn": "по возрастанию",
         "dscn": "по убыванию",
     };
-    const [typesOrder, setTypesOrder] = React.useState(Object.keys(order));
-    const [books, setBooks] = React.useState(getRows([...props.books]));
-    const [type, setType] = React.useState(typesOrder[0]);
+    let [currentPage, setCurrentPage] = React.useState(1);
+    const [pagination, setPagination] = React.useState(getPagination(props.books));
 
-    function getRows(data) {
-        return data.map(book => 
-            <tr key={book.id}>
-                <td>{book.id}</td>
-                <td>{book.name}</td>
-                <td>{book.price}</td>
-            </tr>
-         );
+    function chunk(list, n) {
+        const chunks = [];
+        for (let i = 0; i < list.length; i += n) {
+            chunks.push(list.slice(i, i + n));
+        }
+        return chunks;
     }
 
-    function clickHandlerOrder(event) {
-        const field = event.target.dataset.field;
-        const nextType = event.target.dataset.nextType;
-
-        setBooks(getRows(nextType === "dflt" 
-        ? [...props.books]
-        : [...props.books].sort((a, b) => (nextType === "ascn" ? 1 : -1) * (a[field] - b[field]))));
-
-        setTypesOrder(typesOrder.slice(1).concat(typesOrder.at(0)));
-        setType(typesOrder[1]);
-        console.log(typesOrder)
-        console.log(type)
-
+    function clickPageHandler(event) {
+        setCurrentPage(() => Number(event.target.dataset.page));
+        console.log("change=>", currentPage);
+        setPagination(getPagination(props.books));
     }
+
+    function getPagination(list) {
+        const pages = chunk(list, ipp).map((e, i) => {
+            const current = i + 1 === currentPage ? true : false;
+            return <span className="page" onClick={clickPageHandler} key={i + 1} data-page={i + 1} data-current={current}>{i + 1}</span>
+        });
+        return pages;
+    }
+
+    // useEffect(() => {
+    //     const pages = chunk(props.books, ipp).map((e, i) => {
+    //         <span key={i + 1} data-page={i + 1}>{i + 1}</span>
+    //     });
+    //     setPages(pages);
+    // }, []);
+    // console.log(pages)
+
+    // const [typesOrder, setTypesOrder] = React.useState(Object.keys(order));
+    // const [currentPage, setCurrentPage] = React.useState(1);
+    // const [books, setBooks] = React.useState(getList([...props.books], currentPage));
+    // const [type, setType] = React.useState(typesOrder[0]);
+    // const [pages, setPages] = React.useState(getPages([...props.books]));
+    // console.log(pages)
+    // function getList(data, page) {
+    //     return chunk(data, ipp)[page].map(book => 
+    //         <tr key={book.id}>
+    //             <td>{book.id}</td>
+    //             <td>{book.name}</td>
+    //             <td>{book.price}</td>
+    //         </tr>
+    //      );
+    // }
+
+    // function getPages(data) {        
+    //     return chunk(data, ipp).map((e, i) => <span onClick={clickHandlerPage} key={i + 1} data-page={i + 1} data-current={i + 1 === currentPage}> {i + 1} </span>);
+    // }    
+
+    // function chunk(list, n) {
+    //     const chunks = [];
+    //     for (let i = 0; i < list.length; i += n) {
+    //         chunks.push(list.slice(i, i + n));
+    //     }
+    //     return chunks;
+    // }
+
+    // function clickHandlerPage(event) {        
+    //     setCurrentPage(event.target.dataset.page);
+    //     setBooks(getList([...props.books], currentPage));
+    // }
+
+    // function clickHandlerOrder(event) {
+    //     const field = event.target.dataset.field;
+    //     const nextType = event.target.dataset.nextType;
+
+    //     setBooks(getList(nextType === "dflt" 
+    //     ? [...props.books]
+    //     : [...props.books].sort((a, b) => (nextType === "ascn" ? 1 : -1) * (a[field] - b[field]))), currentPage);
+
+    //     setTypesOrder(typesOrder.slice(1).concat(typesOrder.at(0)));
+    //     setType(typesOrder[1]);
+    // }
 
     return (
-        <React.Fragment>
+        <div className="list">
+            {/* <button data-type={type} data-next-type={typesOrder[1]} data-field="price" onClick={clickHandlerOrder}>{order[type]}</button> */}
             <table>
                 <thead>
                     <tr>
@@ -46,11 +97,14 @@ function Books(props) {
                     </tr>
                 </thead>
                 <tbody>
-                    {books}
-                </tbody>                
+                    {/* {books} */}
+                </tbody>
             </table>
-            <button data-type={type} data-next-type={typesOrder[1]} data-field="price" onClick={clickHandlerOrder}>{order[type]}</button>
-        </React.Fragment>
+            {currentPage}
+            <div className="pagination">
+                {pagination.length > 1 && pagination}
+            </div>
+        </div>
     );
 }
 
